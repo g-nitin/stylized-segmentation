7bd8c384fab804a11a8a02d6149536e1bda7ad1c
niting <niting1209@gmail.com>
2024-05-05 17:52:40 -0400

    Updating model installation

--- segmentation.py
+++ segmentation.py
@@ -6,9 +6,43 @@ from os.path import exists, join
 from subprocess import run
 from typing import List
 from streamlit import cache_data
+import requests
 
 
 def init():
+    # Create the "models" directory if it doesn't exist
+    Path('./models').mkdir(parents=True, exist_ok=True)
+
+    # Define the URL and the local file path
+    # !wget - nc -P. / models / https: // dl.fbaipublicfiles.com / segment_anything / sam_vit_h_4b8939.pth
+    # !wget - nc - P. / models / https: // dl.fbaipublicfiles.com / segment_anything / sam_vit_b_01ec64.pth
+    url = "https://dl.fbaipublicfiles.com/segment_anything/sam_vit_b_01ec64.pth"
+    model_file = Path("models", "sam_vit_b_01ec64.pth")  # Use Path object
+
+    # Check if the file already exists
+    if not model_file.exists():
+        # Download the file using requests
+        response = requests.get(url, stream=True)
+
+        if response.status_code == 200:
+            # Create the file in write-binary mode
+            with model_file.open("wb") as f:
+                for chunk in response.iter_content(1024):
+                    if chunk:  # filter out keep-alive new chunks
+                        f.write(chunk)
+            # print("Downloaded sam_vit_b_01ec64.pth successfully!")
+        else:
+            print(f"Download failed with status code: {response.status_code}")
+
+    # Downloading SAM has been moved to `requirements.txt`
+
+    device: str = 'cuda:0' if torch.cuda.is_available() else 'cpu'
+    model_type: str = "vit_b"
+
+    return model_file, model_type, device
+
+
+def init_old():
     Path('./models').mkdir(exist_ok=True)
 
     # Define the URL and the local file path
